2f78ad1717c2aa299edfca6b297732249c45f439
Sean Harrell <seanvharrell@gmail.com>
2020-03-20 04:07:36 +0000

    calculator test working

---
+++ test/unit/calculator.spec.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const Calculator = require('../../src/js/lib/Calculator');
+
+describe('Calculator', () => {
+  let calculator = null;
+
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+
+  it('should have an add function', () => {
+    expect(calculator.add).to.exist;
+  });
+
+  it('should add 2 + 2 together correctly', () => {
+    expect(calculator.add(2, 2)).to.equal(4);
+  });
+
+  it('should subtract 2 - 2 together correctly',() => {
+    expect(calculator.subtract(2, 2)).to.equal(0);
+  });
+
+  it('should multiply 2 x 2 together correctly',() => {
+    expect(calculator.multiply(2, 2)).to.equal(4);
+  });
+
+  it('should divide 2 / 2 together correctly',() => {
+    expect(calculator.divide(2, 2)).to.equal(1);
+  });
+
+});
---
+++ test/unit/mortgage.spec.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const Mortgage = require('../../src/js/lib/Mortgage');
+
+describe('Mortgage', () => {
+  let mortgageCalculator = null;
+
+  beforeEach(() => {
+    mortgageCalculator = new mortgageCalculator();
+  });
+
+
+
+
+
+
+
+
+});
